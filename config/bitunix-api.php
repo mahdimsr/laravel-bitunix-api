21f90416973a539cd6b6021a835f548e877462fa
mahdi msr <mahdi.msr4@gmail.com>
2025-09-29 03:46:30 +0330

    dev: FIX style

--- config/bitunix-api.php
+++ config/bitunix-api.php
@@ -3,7 +3,7 @@
 return [
 
     'future_base_uri' => 'https://fapi.bitunix.com/',
-    'api_key' => env('BITUNIX_API_KEY'),
-    'api_secret' => env('BITUNIX_API_SECRET'),
+    'api_key' => '',
+    'api_secret' => '',
     'language' => 'en-US',
 ];
